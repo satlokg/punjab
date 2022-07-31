1c6624716cbb0ac6f59f0c5993fba95875f4bf63
satlok <satlok.gupta@gmail.com>
2022-08-01 00:37:52 +0530

    sfds

--- resources/views/layouts/newUser/layout.blade.php
+++ resources/views/layouts/newUser/layout.blade.php
@@ -8,7 +8,7 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<!-- Title Tag  -->
-    <title>Eshop - eCommerce HTML5 Template.</title>
+    <title>Welcome to makeinpunjab.</title>
 	<!-- Favicon -->
 	<link rel="icon" type="image/png" href="{{ asset('public/new/images/favicon.png')}}">
 	<!-- Web Font -->
